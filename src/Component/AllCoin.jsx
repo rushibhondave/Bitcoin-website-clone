e7e185ec4b6ebfafdda3067982f5362ba1973f05
Rushi <rushikesh.21910052@viit.ac.in>
2023-07-22 23:09:48 +0530

    .

--- src/Component/AllCoin.jsx
+++ src/Component/AllCoin.jsx
@@ -13,7 +13,7 @@ function AllCoin() {
     try {
       const fetchCoins = async () => {
         const { data } = await axios.get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=500&page=1&sparkline=false&locale=en"
+         // "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=500&page=1&sparkline=false&locale=en"
         );
         setCoin(data);
         setloadingCoinn(false);
